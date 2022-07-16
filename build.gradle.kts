39a62d47f1bd3207cd46706d94f1e623aa83e9d1
hfhbd <hfhbd@users.noreply.github.com>
2022-07-16 13:51:43 +0200

    Fix AS

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,13 @@ plugins {
     `maven-publish`
     signing
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
-    id("app.cash.sqldelight") version "2.0.0-alpha03" apply false
+    id("app.cash.sqldelight") version "2.0.0-SNAPSHOT" apply false
 }
 
 allprojects {
     repositories {
         mavenCentral()
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
     }
 
     group = "app.softwork"
