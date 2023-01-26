a88c04278802f2674d8ebed5830d5b5b9f26f665
hfhbd <hfhbd@users.noreply.github.com>
2023-01-26 13:18:21 +0100

    Create a new result class when using INTO :FOO

--- sqldelight-db2-dialect/src/main/kotlin/app/softwork/sqldelight/db2dialect/mixins/Db2CompoundSelectMixin.kt
+++ sqldelight-db2-dialect/src/main/kotlin/app/softwork/sqldelight/db2dialect/mixins/Db2CompoundSelectMixin.kt
@@ -1,12 +1,15 @@
 package app.softwork.sqldelight.db2dialect.mixins
 
+import app.cash.sqldelight.dialect.api.*
+import app.softwork.sqldelight.db2dialect.grammar.psi.*
 import com.alecstrong.sql.psi.core.psi.*
 import com.alecstrong.sql.psi.core.psi.impl.*
 import com.intellij.lang.*
 import com.intellij.psi.util.*
 
-internal abstract class Db2CompoundSelectMixin(node: ASTNode): SqlCompoundSelectStmtImpl(node) {
+internal abstract class Db2CompoundSelectMixin(node: ASTNode) : SqlCompoundSelectStmtImpl(node) {
     private val node = PsiTreeUtil.getChildOfType(this, SqlCompoundSelectStmt::class.java)
+
     override fun getCompoundOperatorList(): List<SqlCompoundOperator> = PsiTreeUtil.getChildrenOfTypeAsList(
         node,
         SqlCompoundOperator::class.java
@@ -28,4 +31,21 @@ internal abstract class Db2CompoundSelectMixin(node: ASTNode): SqlCompoundSelect
     )
 
     override fun getWithClause(): SqlWithClause? = PsiTreeUtil.getChildOfType(node, SqlWithClause::class.java)
+
+    fun queryWithResults(): QueryWithResults {
+        for (stmt in selectStmtList) {
+            stmt as Db2SelectStmt
+            if (stmt.selectIntoClause != null) {
+                return CreateNewTableClass(this)
+            }
+        }
+        return SelectQueryable(this)
+    }
+
+    private class CreateNewTableClass(
+        override val select: SqlCompoundSelectStmt,
+    ) : QueryWithResults {
+        override var statement: SqlAnnotatedElement = select
+        override val pureTable: NamedElement? = null
+    }
 }
