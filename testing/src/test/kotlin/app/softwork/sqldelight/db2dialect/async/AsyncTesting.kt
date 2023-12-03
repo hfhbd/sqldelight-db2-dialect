d90f60cd6153a2a5e6655e3ef681a43b7e30b30c
hfhbd <hfhbd@users.noreply.github.com>
2023-12-03 10:50:18 +0100

    Add support for R2DBC

--- testing/src/test/kotlin/app/softwork/sqldelight/db2dialect/async/AsyncTesting.kt
+++ testing/src/test/kotlin/app/softwork/sqldelight/db2dialect/async/AsyncTesting.kt
@@ -1,5 +1,6 @@
 package app.softwork.sqldelight.db2dialect.async
 
+import app.cash.sqldelight.async.coroutines.awaitAsList
 import app.softwork.sqldelight.db2dialect.testR2dbcDriver
 import kotlin.test.*
 
@@ -9,11 +10,11 @@ class AsyncTesting {
         TestingDBAsync.Schema.create(driver)
         val db = TestingDBAsync(driver)
 
-        assertEquals(emptyList(), db.fooQueries.getAll().executeAsList())
+        assertEquals(emptyList(), db.fooQueries.getAll().awaitAsList())
         db.fooQueries.new(Foo(42, "Foo"))
-        assertEquals(listOf(Foo(42, "Foo")), db.fooQueries.getAll().executeAsList())
+        assertEquals(listOf(Foo(42, "Foo")), db.fooQueries.getAll().awaitAsList())
 
         db.fooQueries.create(Foo(100, "Bar"))
-        assertEquals(listOf(Foo(42, "Foo"), Foo(100, "Bar")), db.fooQueries.getAll().executeAsList())
+        assertEquals(listOf(Foo(42, "Foo"), Foo(100, "Bar")), db.fooQueries.getAll().awaitAsList())
     }
 }
