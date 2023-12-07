f2abeb6040b490524ee30ff8369d776619c101e9
hfhbd <hfhbd@users.noreply.github.com>
2023-12-07 19:15:28 +0100

    Add support for CURRENT TIMESTAMP

--- testing/src/test/kotlin/app/softwork/sqldelight/db2dialect/Testing.kt
+++ testing/src/test/kotlin/app/softwork/sqldelight/db2dialect/Testing.kt
@@ -1,6 +1,7 @@
 package app.softwork.sqldelight.db2dialect
 
 import app.softwork.sqldelight.db2dialect.DB2.driver
+import java.time.LocalDateTime
 import kotlin.test.*
 
 class Testing {
@@ -10,10 +11,13 @@ class Testing {
         val db = TestingDB(driver)
 
         assertEquals(emptyList(), db.fooQueries.getAll().executeAsList())
-        db.fooQueries.new(Foo(42, "Foo"))
-        assertEquals(listOf(Foo(42, "Foo")), db.fooQueries.getAll().executeAsList())
+        db.fooQueries.new(Foo(42, -1, LocalDateTime.MIN, "Foo"))
+        assertEquals(listOf(Foo(42, -1, LocalDateTime.MIN, "Foo")), db.fooQueries.getAll().executeAsList())
 
-        db.fooQueries.create(Foo(100, "Bar"))
-        assertEquals(listOf(Foo(42, "Foo"), Foo(100, "Bar")), db.fooQueries.getAll().executeAsList())
+        db.fooQueries.create(Foo(100, -1, LocalDateTime.MIN, "Bar"))
+        assertEquals(
+            listOf(Foo(42, -1, LocalDateTime.MIN, "Foo"), Foo(100, -1, LocalDateTime.MIN, "Bar")),
+            db.fooQueries.getAll().executeAsList()
+        )
     }
 }
